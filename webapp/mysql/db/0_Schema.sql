7b22a6a46affc6f36b949fa843dc2659072ffe04
k-nasa <htilcs1115@gmail.com>
2022-07-03 12:36:27 +0900

    fix

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE isuumo.estate
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
     popularity  INTEGER             NOT NULL
-    INDEX (popularity, rent)
+    INDEX (rent)
 );
 
 CREATE TABLE isuumo.chair
