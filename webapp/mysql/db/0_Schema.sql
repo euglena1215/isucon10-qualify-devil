eed34c26d47bed6de416a38a2766d837f85847db
k-nasa <htilcs1115@gmail.com>
2022-07-03 14:26:29 +0900

    update

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -18,8 +18,7 @@ CREATE TABLE isuumo.estate
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
     popularity  INTEGER             NOT NULL,
-    INDEX idx_popularity (popularity DESC, id ASC),
-    INDEX idx_popularity (door_width, door_height)
+    INDEX idx_popularity (popularity DESC, id ASC)
 );
 
 CREATE TABLE isuumo.chair
