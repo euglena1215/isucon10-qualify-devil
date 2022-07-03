45b8c40ef50f429ec9c48d12e015d10899c079ad
k-nasa <htilcs1115@gmail.com>
2022-07-03 13:23:07 +0900

    fix

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -410,7 +410,7 @@ class App < Sinatra::Base
 
     sqlprefix = 'SELECT * FROM estate WHERE '
     search_condition = search_queries.join(' AND ')
-    limit_offset = " ORDER BY popularity DESC, id ASC LIMIT #{per_page} OFFSET #{per_page * page}" # XXX:
+    limit_offset = " ORDER BY popularity DESC, id ASC LIMIT #{per_page} OFFSET #{per_page * page}"
     count_prefix = 'SELECT COUNT(*) as count FROM estate WHERE '
 
     count = db.xquery("#{count_prefix}#{search_condition}", query_params).first[:count]
